e8dfd6479bf970f288751d9681edfc4606f1b050
lucaramirezo <lucaramirezol@gmail.com>
2023-06-04 19:37:08 +0200

    Modificación nombres variables y orden general

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from PySide6 import QtCore, QtWidgets
 # You need to run the following command to generate the ui_menu.py file
 #     pyside6-uic menu.ui -o ui_menu.py, or
 #     pyside2-uic menu.ui -o ui_menu.py
-from ui_menu import Ui_MainWindow
+from ui_AdminMainView import Ui_AdminMainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_AdminMainWindow()
         self.ui.setupUi(self)
 # Eliminar barra de título y establecer opacidad
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
@@ -27,20 +27,20 @@ class MainWindow(QMainWindow):
                 self.move(self.pos() + event.globalPos() - self.dragPos)
                 self.dragPos = event.globalPos()
                 event.accept()
-        self.ui.frame_superior.mouseMoveEvent = mouseMoveEvent
+        self.ui.admin_main_panel.mouseMoveEvent = mouseMoveEvent
         # Acceder a las páginas
-        self.ui.bt_inicio.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.pageTask))
-        self.ui.bt_uno.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.pageBudget))
-        self.ui.bt_dos.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.pageUsers))
-        self.ui.bt_tres.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.pageSettings))
-        self.ui.bt_cuatro.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.pageHelp))
-        self.ui.bt_cinco.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_cinco))
+        self.ui.bt_task.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.task_card))
+        self.ui.bt_budget.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.budget_card))
+        self.ui.bt_users.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.users_card))
+        self.ui.bt_settings.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.settings_card))
+        self.ui.bt_help.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.help_card))
+        self.ui.bt_log_out.clicked.connect(lambda: self.ui.central_card_container.setCurrentWidget(self.ui.lo_out_card))
         # Control de la barra de título
-        self.ui.bt_minimizar.clicked.connect(self.control_bt_minimizar)
-        self.ui.bt_restaurar.clicked.connect(self.control_bt_normal)
-        self.ui.bt_maximizar.clicked.connect(self.control_bt_maximizar)
-        self.ui.bt_cerrar.clicked.connect(self.close)
-        self.ui.bt_restaurar.hide()
+        self.ui.bt_min.clicked.connect(self.control_bt_minimizar)
+        self.ui.bt_restore.clicked.connect(self.control_bt_normal)
+        self.ui.bt_max.clicked.connect(self.control_bt_maximizar)
+        self.ui.bt_close.clicked.connect(self.close)
+        self.ui.bt_restore.hide()
 
         # Menú lateral
 
@@ -51,27 +51,48 @@ class MainWindow(QMainWindow):
 
     def control_bt_normal(self):
         self.showNormal()
-        self.ui.bt_restaurar.hide()
-        self.ui.bt_maximizar.show()
+        self.ui.bt_restore.hide()
+        self.ui.bt_max.show()
 
     def control_bt_maximizar(self):
         self.showMaximized()
-        self.ui.bt_maximizar.hide()
-        self.ui.bt_restaurar.show()
+        self.ui.bt_max.hide()
+        self.ui.bt_restore.show()
 
     def mover_menu(self):
-        width = self.ui.frame_lateral.width()
+        width = self.ui.menu_bar.width()
         normal = 0
         if width == 0:
             extender = 200
         else:
             extender = normal
-        self.animacion = QtCore.QPropertyAnimation(self.ui.frame_lateral, b'minimumWidth')
+
+        self.animacion = QtCore.QPropertyAnimation(self.ui.menu_bar, b'minimumWidth')
         self.animacion.setDuration(350)
         self.animacion.setStartValue(width)
         self.animacion.setEndValue(extender)
         self.animacion.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
         self.animacion.start()
+
+        # Desplazar el contenido hacia la izquierda
+        content_position = self.ui.menu_bar.pos()
+        content_width = self.ui.menu_bar.width()
+        if extender == 0:
+            # Ocultar el menú lateral, desplazar el contenido hacia la izquierda
+            self.animacion_content = QtCore.QPropertyAnimation(self.ui.menu_bar, b'pos')
+            self.animacion_content.setDuration(350)
+            self.animacion_content.setStartValue(content_position)
+            self.animacion_content.setEndValue(QtCore.QPoint(-extender, content_position.y()))
+            self.animacion_content.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animacion_content.start()
+        else:
+            # Mostrar el menú lateral, desplazar el contenido hacia la derecha
+            self.animacion_content = QtCore.QPropertyAnimation(self.ui.menu_bar, b'pos')
+            self.animacion_content.setDuration(350)
+            self.animacion_content.setStartValue(content_position)
+            self.animacion_content.setEndValue(QtCore.QPoint(normal, content_position.y()))
+            self.animacion_content.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animacion_content.start()
 
     ## SizeGrip
     def resizeEvent(self, event):
